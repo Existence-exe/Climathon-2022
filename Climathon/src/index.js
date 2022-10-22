4d2148c2a310395cddf4c3a998932d736da7d3d9
Existence.exe <pritishdutta20@gmail.com>
2022-10-23 02:08:25 +0530

    hmm

---
+++ Climathon/src/index.js
@@ -0,0 +1,11 @@
+AOS.init();
+
+
+ityped.init('#topic', {
+    strings:['Climathon 2022', 'Project by Team', 'Alohomora'],
+    startDelay: 500,
+    loop: true,
+    typeSpeed:  80,
+    backSpeed : 60,
+    cursorChar: "|"
+});
